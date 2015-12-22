2ef3cc7f3a97ca342c262bf6fe6e841732a40de4
abel <abel.duan@hkgreenwood.com>
2015-12-22 11:44:50 +0800

    ad seo

--- js/agency.js
+++ js/agency.js
@@ -6,45 +6,69 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
 
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
 
-    $(window).scroll(function() {
-      	if (isScrolledIntoView("section#services")) { window.history.pushState("state", "title", "hkgreenwoodsd/index.html/services"); return; }
-      	if (isScrolledIntoView("section#portfolio")) { window.history.pushState("state", "title", "hkgreenwoodsd/index.html/portfolio"); return; }
-      	if (isScrolledIntoView("section#partner")) { window.history.pushState("state", "title", "hkgreenwoodsd/index.html/partner"); return; }
-      	if (isScrolledIntoView("section#about")) { window.history.pushState("state", "title", "hkgreenwoodsd/index.html/about"); return; }
-        if (isScrolledIntoView("section#team")) { window.history.pushState("state", "title", "hkgreenwoodsd/index.html/team"); return; }
-        if (isScrolledIntoView("section#contact")) { window.history.pushState("state", "title", "hkgreenwoodsd/index.html/contact"); return; }
-    });
+    if (isScrolledIntoView("section#services")) {
+      window.history.pushState("state", "", "#services");
+      return;
+    }
+    if (isScrolledIntoView("section#portfolio")) {
+      window.history.pushState("state", "", "#portfolio");
+      return;
+    }
+    if (isScrolledIntoView("section#partner")) {
+      window.history.pushState("state", "", "#partner");
+      return;
+    }
+    if (isScrolledIntoView("section#about")) {
+      window.history.pushState("state", "", "#about");
+      return;
+    }
+    if (isScrolledIntoView("section#team")) {
+      window.history.pushState("state", "", "#team");
+      return;
+    }
+    if (isScrolledIntoView("section#contact")) {
+      window.history.pushState("state", "", "#contact");
+      return;
+    }
+  });
+
+  // $(window).scroll(function() {
+  //   	if (isScrolledIntoView("section#services")) { window.history.pushState("state", "title", "/hkgreenwoodsd/index.html/services"); return; }
+  //   	if (isScrolledIntoView("section#portfolio")) { window.history.pushState("state", "title", "/hkgreenwoodsd/index.html/portfolio"); return; }
+  //   	if (isScrolledIntoView("section#partner")) { window.history.pushState("state", "title", "/hkgreenwoodsd/index.html/partner"); return; }
+  //   	if (isScrolledIntoView("section#about")) { window.history.pushState("state", "title", "/hkgreenwoodsd/index.html/about"); return; }
+  //     if (isScrolledIntoView("section#team")) { window.history.pushState("state", "title", "/hkgreenwoodsd/index.html/team"); return; }
+  //     if (isScrolledIntoView("section#contact")) { window.history.pushState("state", "title", "/hkgreenwoodsd/index.html/contact"); return; }
+  // });
 });
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
-    target: '.navbar-fixed-top'
+  target: '.navbar-fixed-top'
 })
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+  $('.navbar-toggle:visible').click();
 });
 
-function isScrolledIntoView(elem)
-{
-    var $elem = $(elem);
-    var $window = $(window);
+function isScrolledIntoView(elem) {
+  var $elem = $(elem);
+  var $window = $(window);
 
-    var docViewTop = $window.scrollTop();
-    var docViewBottom = docViewTop + $window.height();
+  var docViewTop = $window.scrollTop();
+  var docViewBottom = docViewTop + $window.height();
 
-    var elemTop = $elem.offset().top;
-    var elemBottom = elemTop + $elem.height();
+  var elemTop = $elem.offset().top;
+  var elemBottom = elemTop + $elem.height();
 
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
